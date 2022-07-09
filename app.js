29f844981731b11ab9eaef06e70b2a2fe4d87383
Salomon Cohen <Salomon.Cohen@students.dominican.edu>
2022-07-09 19:33:43 -0400

    Users can put down, but its slow

--- app.js
+++ app.js
@@ -18,23 +18,49 @@ server.listen(3000, () => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on('joinGame', (gameId) => {
-        console.log('User joining game', gameId);
-        const game = {boxes : []}
-        let data = JSON.stringify(game);
-        fs.writeFileSync(gameId+'.json', data);
-       
-        fs.watchFile("./"+gameId+'.json', (curr, prev) => {
-            let jsonData = require('./games/gameId.json');
-            console.log(jsonData);
-            io.emit('gameUpdate', 
-                jsonData);
+    socket.on('getGame', (gameId) => {
+        fs.readFile(gameId+'.json', (err, data) => {
+            if (err) throw err;
+            let game = JSON.parse(data);
+            io.emit('gameUpdate', game);
+
         });
+
         
       });
-      socket.on('makeMove', (gameId, moveLocation, moveType) => {
-        console.log('User made a move');
+    
 
+    socket.on('joinGame', (gameId) => {
+        console.log('User joining game', gameId);
+        const game = {boxes : [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,]}
+        let data = JSON.stringify(game);
+        fs.writeFileSync(gameId+'.json', data);
+        io.emit('ready');
+        fs.watchFile("./"+gameId+'.json', (curr, prev) => {
+            io.emit('ready');
+    
+        });
+        
+
+        
+      });
+      socket.on('makeMove', (data) => {
+        console.log('User made a move', data);
+        fs.readFile(data.gameId+'.json', (err, game) => {
+            if (err) throw err;
+            game = JSON.parse(game);
+            if(!game.boxes[data.moveLocation]){
+                game.boxes[data.moveLocation] = data.moveType
+                let newData = JSON.stringify(game);
+                fs.writeFileSync(data.gameId+'.json', newData);
+    
+                
+            }
+
+        });
+        //EMIT EVENT THAT YOU CANNOT MOVE THERE
+        
+        
         });
    
     socket.on('disconnect', () => {
--- static/client.js
+++ static/client.js
@@ -6,3 +6,32 @@ function joinGame(gameId){
     socket.emit('joinGame',gameId)
 
 }
+
+function makeMove(moveLocation){
+    var gameId = document.getElementById('gameId').value
+    var moveType
+    if(document.getElementById('xType').checked){
+        moveType = "X"
+    }else{
+        moveType = "O"
+    }
+    console.log("Making a move",{gameId, moveType, moveLocation})
+    socket.emit('makeMove',{gameId, moveType, moveLocation})
+
+}
+
+socket.on('gameUpdate',(data) => {
+    console.log("There is a game update", data)
+    for(var i=0;i< data.boxes.length;i++){
+        if(data.boxes[i]){
+            document.getElementById(i.toString()).innerText = data.boxes[i]
+        }
+    }
+
+
+});
+
+
+socket.on('ready',()=>{
+    socket.emit('getGame',document.getElementById('gameId').value)
+})
